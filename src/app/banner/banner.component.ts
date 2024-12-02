9c514817bf4159d88e2687adcca2b2be8bda2556
AliQureshi24 <ali2000qureshi@gmail.com>
2024-12-03 00:38:26 +0500

    feat: complete test 3

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  imports: [],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.css',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+})
+export class BannerComponent {
+
+}
